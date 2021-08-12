52d8dd096b7bf9ebb52fe4b8dfe50801c065485c
LaughingZhu <474268433@qq.com>
2021-08-12 17:21:45 +0800

    国际化相关代码删除

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,33 +1,23 @@
+/*
+ * @Description:
+ * @Author: LaughingZhu
+ * @Date: 2020-09-07 21:46:05
+ * @LastEditros:
+ * @LastEditTime: 2021-08-12 17:21:08
+ */
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import Cookies from 'js-cookie'
-import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
 import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
-import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
-import elementJaLocale from 'element-ui/lib/locale/lang/ja'// element-ui lang
-import enLocale from './en'
+
 import zhLocale from './zh'
-import esLocale from './es'
-import jaLocale from './ja'
 
 Vue.use(VueI18n)
 
 const messages = {
-  en: {
-    ...enLocale,
-    ...elementEnLocale
-  },
   zh: {
     ...zhLocale,
     ...elementZhLocale
-  },
-  es: {
-    ...esLocale,
-    ...elementEsLocale
-  },
-  ja: {
-    ...jaLocale,
-    ...elementJaLocale
   }
 }
 export function getLanguage() {
