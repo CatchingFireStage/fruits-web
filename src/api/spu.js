affda92e1dcbc07038273e603307501742d1c093
LaughingZhu <474268433@qq.com>
2021-08-18 09:38:34 +0800

    SPU分类 编辑/添加/删除接口

--- src/api/spu.js
+++ src/api/spu.js
@@ -3,18 +3,20 @@
  * @Author: LaughingZhu
  * @Date: 2021-08-17 10:02:16
  * @LastEditros:
- * @LastEditTime: 2021-08-17 18:06:53
+ * @LastEditTime: 2021-08-18 09:36:46
  */
 import request from '@/utils/request'
 
-// export function getCategroyLists(data) {
-//   return request({
-//     url: 'http://106.12.76.73:8081/admin/assist/login',
-//     method: 'post',
-//     data
-//   })
-// }
+// SPU分类-添加
+export function addCategroy(data) {
+  return request({
+    url: 'http://106.12.76.73:8081/admin/spu/category',
+    method: 'post',
+    data
+  })
+}
 
+// SPU分类-列表
 export function getCategoryList(params) {
   return request({
     url: 'http://106.12.76.73:8081/admin/spu/categories',
@@ -23,9 +25,19 @@ export function getCategoryList(params) {
   })
 }
 
+// SPU分类-删除
 export function deleteCategoryById(id) {
   return request({
     url: `http://106.12.76.73:8081/admin/spu/category/${id}`,
     method: 'delete'
+  })
+}
+
+// SPU分类-更新
+export function editCategroy(data, id) {
+  return request({
+    url: `http://106.12.76.73:8081/admin/spu/category/${id}`,
+    method: 'put',
+    data
   })
 }
