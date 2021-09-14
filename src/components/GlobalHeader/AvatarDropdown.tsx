0e814f34f60f3bd0f8520eebc3d84e548661ee2e
LaughingZhu <474268433@qq.com>
2021-09-14 10:19:15 +0800

    国际化库 删除

--- src/components/GlobalHeader/AvatarDropdown.tsx
+++ src/components/GlobalHeader/AvatarDropdown.tsx
@@ -3,66 +3,26 @@
  * @Author: LaughingZhu
  * @Date: 2020-12-22 17:47:16
  * @LastEditros: 
- * @LastEditTime: 2021-06-09 18:00:38
+ * @LastEditTime: 2021-09-14 10:15:28
  */
-import { Avatar, Icon, Menu, Spin } from 'antd';
-import { ClickParam } from 'antd/es/menu';
-import { FormattedMessage } from 'umi-plugin-react/locale';
+import { Avatar, Spin } from 'antd';
 import React from 'react';
-import { connect } from 'dva';
-import { router } from 'umi';
 
-import { ConnectProps, ConnectState } from '@/models/connect';
-import { CurrentUser } from '@/models/user';
+import { ConnectProps } from '@/models/connect';
 import HeaderDropdown from '../HeaderDropdown';
 import styles from './index.less';
 
 export interface GlobalHeaderRightProps extends ConnectProps {
-  currentUser?: CurrentUser;
   menu?: boolean;
 }
 
 class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
-  onMenuClick = (event: ClickParam) => {
-    const { key } = event;
 
-    if (key === 'logout') {
-      localStorage.setItem('next_admin', '')
-
-      location.replace('https://sso.zhiyinlou.com/sso/logout?path=https://sso.zhiyinlou.com/portal/login/1191164175')
-      return;
-    }
-    router.push(`/account/${key}`);
-  };
-
-  render(): React.ReactNode {
-    const { currentUser = { avatar: '', name: '' }, menu } = this.props;
-
-    const menuHeaderDropdown = (
-      <Menu className={styles.menu} selectedKeys={[]} onClick={this.onMenuClick}>
-        {menu && (
-          <Menu.Item key="center">
-            <Icon type="user" />
-            <FormattedMessage id="menu.account.center" defaultMessage="account center" />
-          </Menu.Item>
-        )}
-        {menu && (
-          <Menu.Item key="settings">
-            <Icon type="setting" />
-            <FormattedMessage id="menu.account.settings" defaultMessage="account settings" />
-          </Menu.Item>
-        )}
-        {menu && <Menu.Divider />}
-
-        <Menu.Item key="logout">
-          <Icon type="logout" />
-          <FormattedMessage id="menu.account.logout" defaultMessage="logout" />
-        </Menu.Item>
-      </Menu>
-    );
+  render(): React.ReactNode { 
+    const currentUser = { avatar: require('../../assets/avatar.jpeg'), name: '管理员' }
 
     return currentUser && currentUser.name ? (
-      <HeaderDropdown overlay={menuHeaderDropdown}>
+      <HeaderDropdown overlay={<></>}>
         <span className={`${styles.action} ${styles.account}`}>
           <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
           <span className={styles.name}>{currentUser.name}</span>
@@ -73,6 +33,4 @@ class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
     );
   }
 }
-export default connect(({ user }: ConnectState) => ({
-  currentUser: user.currentUser,
-}))(AvatarDropdown);
+export default AvatarDropdown;
--- src/components/GlobalHeader/RightContent.tsx
+++ src/components/GlobalHeader/RightContent.tsx
@@ -3,17 +3,13 @@
  * @Author: LaughingZhu
  * @Date: 2020-12-22 17:47:16
  * @LastEditros: 
- * @LastEditTime: 2021-06-09 17:37:54
+ * @LastEditTime: 2021-09-14 10:07:49
  */
-import { Icon, Tooltip } from 'antd';
 import React from 'react';
 import { connect } from 'dva';
-import { formatMessage } from 'umi-plugin-react/locale';
 import { ConnectProps, ConnectState } from '@/models/connect';
 
 import Avatar from './AvatarDropdown';
-import HeaderSearch from '../HeaderSearch';
-import SelectLang from '../SelectLang';
 import styles from './index.less';
 
 export type SiderTheme = 'light' | 'dark';
@@ -32,46 +28,8 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
 
   return (
     <div className={className}>
-      {/* <HeaderSearch
-        className={`${styles.action} ${styles.search}`}
-        placeholder={formatMessage({
-          id: 'component.globalHeader.search',
-        })}
-        defaultValue="umi ui"
-        dataSource={[
-          formatMessage({
-            id: 'component.globalHeader.search.example1',
-          }),
-          formatMessage({
-            id: 'component.globalHeader.search.example2',
-          }),
-          formatMessage({
-            id: 'component.globalHeader.search.example3',
-          }),
-        ]}
-        onSearch={value => {
-          console.log('input', value);
-        }}
-        onPressEnter={value => {
-          console.log('enter', value);
-        }}
-      /> */}
-      <Tooltip
-        title={formatMessage({
-          id: 'component.globalHeader.help',
-        })}
-      >
-        <a
-          target="_blank"
-          href="https://pro.ant.design/docs/getting-started"
-          rel="noopener noreferrer"
-          className={styles.action}
-        >
-          <Icon type="question-circle-o" />
-        </a>
-      </Tooltip>
+
       <Avatar />
-      {/* <SelectLang className={styles.action} /> */}
     </div>
   );
 };
