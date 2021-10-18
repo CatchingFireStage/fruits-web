259580511e26564fd9398f489ac538be78afc7a3
LaughingZhu <474268433@qq.com>
2021-10-18 17:56:07 +0800

    制作中订单 页面 和 订单样式

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -3,16 +3,19 @@
  * @Author: LaughingZhu
  * @Date: 2021-10-09 17:46:07
  * @LastEditros:
- * @LastEditTime: 2021-10-12 10:19:05
+ * @LastEditTime: 2021-10-18 17:53:05
  */
 import { Button } from 'antd';
 import React, { Component } from 'react';
+import { orderDelivery, orderFulfill } from './service';
 import styles from './style.module.less';
 interface IProps {}
 interface IState {
   socket: any;
   doingList: any; // 制作中
   doneList: any; // 制作完成
+  makedList: any; // 制作中 -> 制作完成的列表，通过array.filter 实现 类似删除的功能，避免左右显示相同的订单
+  finishList: any; // 制作完成 作用同上
 }
 class Order extends Component<IProps, IState> {
   constructor(props: IProps) {
@@ -21,6 +24,8 @@ class Order extends Component<IProps, IState> {
       socket: null,
       doingList: [],
       doneList: [],
+      makedList: [],
+      finishList: [],
     };
   }
   componentDidMount = () => {
@@ -51,22 +56,37 @@ class Order extends Component<IProps, IState> {
       switch (response.eventType) {
         // 已支付订单列表事件
 
+        // 首次进来获取列表
         case 'PAY_ORDER_LIST':
           this.setState({
-            doingList: [...this.state.doingList, ...JSON.parse(response.event)],
+            doingList: JSON.parse(response.event),
           });
           break;
 
         // 已支付订单事件
+        // 新的订单
         case 'PAY_ORDER':
+          const resDoing = Object.assign([], this.state.doingList);
+          resDoing.push(JSON.parse(response.event));
+          this.setState({
+            doingList: resDoing,
+          });
           break;
 
         // 制作完成订单事件
         case 'FULFILL_ORDER_LIST':
+          this.setState({
+            doneList: JSON.parse(response.event),
+          });
           break;
 
         // 制作完成订单列表事件
         case 'FULFILL_ORDER':
+          const resDone = Object.assign([], this.state.doneList);
+          resDone.push(JSON.parse(response.event));
+          this.setState({
+            doneList: resDone,
+          });
           break;
         default:
           break;
@@ -75,19 +95,41 @@ class Order extends Component<IProps, IState> {
     socket.onclose = () => {
       console.log('连接关闭');
     };
+  };
 
-    this.setState({
-      socket,
+  /**
+   * @desc 订单-制作完成
+   * @param id {number} 订单id
+   */
+  orderMaked = (id: number) => {
+    orderFulfill(id, () => {
+      this.setState({
+        makedList: [...this.state.makedList, id],
+      });
     });
   };
 
-  socketSend = (message: any) => {
-    this.state.socket.send(message);
+  /**
+   * @desc 订单-已经送达
+   * @param id {number} 订单id
+   */
+  orderFinished = (id: number) => {
+    orderDelivery(id, () => {
+      this.setState({
+        finishList: [...this.state.finishList, id],
+      });
+    });
   };
 
   render() {
-    console.log(this.state.doingList, this.state.doneList);
-    const { doingList, doneList } = this.state;
+    const { doingList, doneList, makedList, finishList } = this.state;
+    console.log(doneList, makedList, 'reander');
+    const renderDodingList = doingList.filter((item: any) => {
+      return !makedList.includes(item.id);
+    });
+    const renderDoneList = doneList.filter((item: any) => {
+      return !finishList.includes(item.id);
+    });
     return (
       <div className={styles.container}>
         <div className={styles.doing}>
@@ -99,7 +141,7 @@ class Order extends Component<IProps, IState> {
             <span style={{ flexBasis: '20%' }}>操作</span>
           </div>
           <div className={styles.main}>
-            {doingList.map((item: any) => (
+            {renderDodingList.map((item: any) => (
               <div key={`doing-${item.id}`} className={styles.item}>
                 <span className={styles.sort}>{item.id}</span>
                 <div className={styles.box}>
@@ -124,7 +166,11 @@ class Order extends Component<IProps, IState> {
                   )}
                 </div>
                 <div className={styles.tools}>
-                  <Button size="small" className={styles.button}>
+                  <Button
+                    size="small"
+                    className={styles.button}
+                    onClick={() => this.orderMaked(item.id)}
+                  >
                     制作完成
                   </Button>
                 </div>
@@ -134,7 +180,43 @@ class Order extends Component<IProps, IState> {
         </div>
         <div className={styles.done}>
           <div className={styles.title}>完成制作</div>
-          <div className={styles.main}></div>
+          <div className={styles.main}>
+            {renderDoneList.map((item: any) => (
+              <div key={`doing-${item.id}`} className={styles.item}>
+                <span className={styles.sort}>{item.id}</span>
+                <div className={styles.box}>
+                  {item.description.orderDescription.map(
+                    (child: any, index: number) => (
+                      <div key={`spu-${index}`} className={styles.spu}>
+                        <div className={styles.name}>{child.spu.name}</div>
+                        <div className={styles.specifition}>
+                          {child.spuSpecificationValue.map(
+                            (spe: any, spe_Index: number) => (
+                              <div
+                                key={`spe-${spe_Index}`}
+                                className={styles.spe_name}
+                              >
+                                {spe.value}
+                              </div>
+                            ),
+                          )}
+                        </div>
+                      </div>
+                    ),
+                  )}
+                </div>
+                <div className={styles.tools}>
+                  <Button
+                    size="small"
+                    className={styles.button}
+                    onClick={() => this.orderFinished(item.id)}
+                  >
+                    送达完成
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
