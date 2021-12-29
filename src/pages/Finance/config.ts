a749c62448332094bdbfec90e8d9051b9a91b18e
LaughingZhu <474268433@qq.com>
2021-12-29 10:57:34 +0800

    退款状态

--- src/pages/Finance/config.ts
+++ src/pages/Finance/config.ts
@@ -3,7 +3,7 @@
  * @Author: LaughingZhu
  * @Date: 2021-12-02 17:40:20
  * @LastEditros:
- * @LastEditTime: 2021-12-02 17:41:41
+ * @LastEditTime: 2021-12-29 10:36:48
  */
 export const stateArr = [
   // 支付状态,ORDER:下单，SUCCESS已支付,FAIL支付失败,REFUND退款
@@ -24,3 +24,24 @@ export const stateArr = [
     value: 'REFUND',
   },
 ];
+
+
+// 退款的状态
+export const refundState = [
+  {
+    label: '退款中',
+    value: 'REFUND',
+  },
+  {
+    label: '退款成功',
+    value: 'SUCCESS',
+  },
+  {
+    label: '退款异常',
+    value: 'ABNORMAL',
+  },
+  {
+    label: '退款关闭',
+    value: 'CLOSE',
+  }
+]
