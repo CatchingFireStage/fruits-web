9bf71f04ed0e9cad4e6e9864d94dedd4c80f9661
LaughingZhu <474268433@qq.com>
2022-05-05 10:22:33 +0800

    fix(*): 添加优惠券请求字段更正

--- src/pages/Coupon/index.tsx
+++ src/pages/Coupon/index.tsx
@@ -7,21 +7,14 @@
  */
 import { Button, Form, Icon, Modal, PageHeader, Input } from 'antd';
 import React, { useState } from 'react'
-import router from 'umi/router';
 import styles from './style.module.less'
 
-let id = 0;
-interface EditTypes {
-  id: string;
-  name: string;
-  coupon: {name: string}[] | null;
-}
 interface IProps {
   form: any;
 }
 function Coupon (props: IProps) {
   const { form } = props;
-  const { getFieldDecorator, getFieldValue, resetFields } = form;
+  const { getFieldDecorator, resetFields } = form;
   const [editStatus, setEditStatus] = useState(false);
   const [editId, setEditId] = useState('')
   const formItemLayout = {
@@ -40,64 +33,6 @@ function Coupon (props: IProps) {
       sm: { span: 20, offset: 4 },
     },
   };
-  getFieldDecorator('keys', { initialValue: [0] });
-  const keys = getFieldValue('keys');
-
-  const remove = (k: number) => {
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys');
-    // We need at least one passenger
-    if (keys.length === 1) {
-      return;
-    }
-
-    // can use data-binding to set
-    form.setFieldsValue({
-      keys: keys.filter((key: number) => key !== k),
-    });
-  };
-
-  const add = () => {
-    // can use data-binding to get
-    id += 1;
-    const keys = form.getFieldValue('keys');
-    const nextKeys = keys.concat(id);
-    // can use data-binding to set
-    // important! notify form to detect changes
-    form.setFieldsValue({
-      keys: nextKeys,
-    });
-  };
-
-  const formItems = keys.map((k: number, index: number) => (
-    <Form.Item
-      {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-      label={index === 0 ? '优惠内容' : ''}
-      required={true}
-      key={k}
-    >
-      {getFieldDecorator(`couponItem[${k}][name]`, {
-        validateTrigger: ['onChange', 'onBlur'],
-        initialValue: '',
-        rules: [
-          {
-            required: true,
-            whitespace: true,
-            message: "优惠内容为必填项",
-          },
-        ],
-      })(
-        <Input placeholder="请输入优惠内容" style={{ width: '90%', marginRight: 8 }} />
-      )}
-      {index > 0 ? (
-        <Icon
-          className="dynamic-delete-button"
-          type="minus-circle-o"
-          onClick={() => remove(k)}
-        />
-      ) : null}
-    </Form.Item>
-  ));
 
   // 分页配置
   // const paginationConfig: PaginationConfig = {
@@ -116,13 +51,8 @@ function Coupon (props: IProps) {
     e && e.preventDefault();
     props.form.validateFields((err: any, values: any) => {
       if (!err) {
-        const { couponItem, name } = values;
-        const items = couponItem.filter((item: {name: string}) => item);
-        const formData = {
-          name,
-          couponItem: items,
-        }
-        console.log(formData, 'formData------')
+
+        console.log(values, 'formData------')
       }
     });
   };
@@ -163,32 +93,44 @@ function Coupon (props: IProps) {
         footer={null}
       >
         <div className={styles.edit}>
-          
           <Form onSubmit={handleSubmit}>
             <Form.Item
               {...formItemLayout}
-              label={'优惠名称'}
+              label={'满'}
               required
             >
-              {getFieldDecorator(`name`, {
+              {getFieldDecorator(`discounts`, {
                 validateTrigger: ['onChange', 'onBlur'],
                 rules: [
                   {
                     required: true,
                     whitespace: true,
-                    message: "优惠名称为必填项",
+                    message: "满多少元",
                   },
                 ],
               })(
-                <Input placeholder="请输入优惠名称" style={{ width: '90%', marginRight: 8 }} />
+                <Input type='number' placeholder="请输入满多少元" style={{ width: '90%', marginRight: 8 }} />
               )}
               
             </Form.Item>
-            {formItems}
-            <Form.Item {...formItemLayoutWithOutLabel} >
-              <Button type="dashed" onClick={add} style={{ width: '90%' }}>
-                <Icon type="plus" /> 添加优惠内容
-              </Button>
+            <Form.Item
+              {...formItemLayout}
+              label={'减'}
+              required
+            >
+              {getFieldDecorator(`waterLine`, {
+                validateTrigger: ['onChange', 'onBlur'],
+                rules: [
+                  {
+                    required: true,
+                    whitespace: true,
+                    message: "减多少元",
+                  },
+                ],
+              })(
+                <Input type='number' placeholder="请输入减多少元" style={{ width: '90%', marginRight: 8 }} />
+              )}
+              
             </Form.Item>
             <Form.Item {...formItemLayoutWithOutLabel} >
               <Button type="primary" htmlType="submit" style={{ width: '90%' }}>
